1de34b0a12d6b48d29d5f25819b3e3f09fd15a43
Hera <heraprasetyo@gmail.com>
2022-07-25 21:05:41 +0700

    2

---
+++ src/components/CarList/index.jsx
@@ -0,0 +1,7 @@
+export default function CarList(){
+    return(
+        <>
+        <h1>car list</h1>
+        </>
+    )
+}
---
+++ src/components/SectionService/index.jsx
@@ -0,0 +1,7 @@
+export default function SectionService(){
+    return(
+        <>
+        <h1>Section service</h1>
+        </>
+    )
+}
