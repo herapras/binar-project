e422537bc21a40d053c91dc804cdf023a2e92358
Hera <heraprasetyo@gmail.com>
2022-07-26 21:46:00 +0700

    bersamaTue

--- src/components/CarList/index.jsx
+++ src/components/CarList/index.jsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
+
 export default function CarList(){
     return(
         <>
         <h1>car list</h1>
+        <Button variant='contained' onClick={() => handleViewDetail(car.id)} />
         </>
     )
 }
---
+++ src/components/Skeleton/index.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+
+export default fuction SkeletonCard() {
+    return (
+        <Stack spacing={1}>
+            <Card style={{width='333', height='478'}}>
+                <Skeleton variant="rectangular" width={270} height={160} />
+                <Skeleton variant="rectangular" width={285} height={20} />
+                <Skeleton variant="rectangular" width={285} height={24} />
+                <Skeleton variant="rectangular" width={310} height={60} />
+                <Skeleton variant="rectangular" width={285} height={48} />
+            </Card>
+        </Stack>
+    )
+}
